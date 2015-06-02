098083737eeb52bf64fd160ca02dca10f08b7021
Jonathan Mathews <jdmathews@gmail.com>
2015-06-02 23:49:53 +0100

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Myapp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'spokesdev.herokuapp.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'spokesdev.herokuapp.com'}
 end
