825fb8c11af7c22abc2baa2156b0947dc4965632
Jonathan Mathews <jdmathews@gmail.com>
2015-06-03 00:33:14 +0100

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Myapp::Application.routes.draw do
   root 'home#index'
-  devise_for :users, :controllers => { :registrations => "registrations" }
+  devise_for :users
   resources :merchants, except: :show
 
   resources :photos
